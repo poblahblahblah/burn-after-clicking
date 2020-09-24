ff8562ce5693726aa608d43bb02a20b21cb3f012
Patrick O’Brien <patrick.obrien@thetradedesk.com>
2020-09-24 15:03:33 -0700

    disabling protect against forgery

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,8 @@ module BurnAfterClicking
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.action_controller.forgery_protection_origin_check = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
